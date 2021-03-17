d1d84234fe5637a027a6e3690d10f5c09c186399
Starchenkov <Starchenkov@live.ru>
2021-03-17 22:23:46 +0700

    much-more-items (with: firstIndex)

--- Test/ViewModel.swift
+++ Test/ViewModel.swift
@@ -33,60 +33,47 @@ class ViewModel: NSObject {
     }
     
     func configCell(cell: UITableViewCell, typeCell: String) -> UITableViewCell {
+        guard let index = elementsArray.firstIndex(where: { (item) -> Bool in
+            item.name == typeCell
+        }) else { return cell }
+        let element = elementsArray[index]
         
         switch typeCell {
         case "hz":
-            for index in 0..<elementsArray.count {
-                let element = elementsArray[index]
-                if element.name == "hz" {
-                    let newCell = cell as! hzTableViewCell
-                    newCell.label.text = element.data.text
-                    elementsArray.remove(at: index)
-                    return newCell
-                }
-            }
+            let newCell = cell as! hzTableViewCell
+            newCell.label.text = element.data.text
+            elementsArray.remove(at: index)
+            return newCell
+            
         case "selector":
-            for index in 0..<elementsArray.count {
-                let element = elementsArray[index]
-                if element.name == "selector" {
-                    let newCell = cell as! selectorTableViewCell
-                    newCell.selector.removeAllSegments()
-                    if let variants = element.data.variants {
-                        for index in 0..<variants.count {
-                            let variant = variants[index]
-                            newCell.selector.insertSegment(withTitle: variant.text, at: variant.id, animated: false)
-                            if variant.id == element.data.selectedId {
-                                newCell.selector.selectedSegmentIndex = index
-                            }
-                        }
+            let newCell = cell as! selectorTableViewCell
+            newCell.selector.removeAllSegments()
+            if let variants = element.data.variants {
+                for index in 0..<variants.count {
+                    let variant = variants[index]
+                    newCell.selector.insertSegment(withTitle: variant.text, at: variant.id, animated: false)
+                    if variant.id == element.data.selectedId {
+                        newCell.selector.selectedSegmentIndex = index
                     }
-                    elementsArray.remove(at: index)
-                    return newCell
                 }
             }
+            elementsArray.remove(at: index)
+            return newCell
+            
         case "picture":
-            for index in 0..<elementsArray.count {
-                let element = elementsArray[index]
-                if element.name == "picture" {
-                    let newCell = cell as! pictureTableViewCell
-                    newCell.label.text = element.data.text
-                    if let urlImage = element.data.url {
-                        networkManager.downloadImage(with: urlImage) { (image) in
-                            newCell.picture.image = image
-                        }
-                    }
-                    elementsArray.remove(at: index)
-                    return newCell
+            let newCell = cell as! pictureTableViewCell
+            newCell.label.text = element.data.text
+            if let urlImage = element.data.url {
+                networkManager.downloadImage(with: urlImage) { (image) in
+                    newCell.picture.image = image
                 }
             }
+            elementsArray.remove(at: index)
+            return newCell
+            
         default:
             break
         }
         return cell
-    }
-    
-    
-    
-    
-    
+    }   
 }
